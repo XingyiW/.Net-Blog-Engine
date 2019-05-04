1f1104bf00a989c3784e30c713b8bf00d59d397a
XingyiW <46615477+XingyiW@users.noreply.github.com>
2019-05-04 00:24:32 -0400

    Add files via upload

---
+++ Lab6/Models/Tweet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6.Models
+{
+    public class Tweet
+    {
+        public int TweetId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The person who created the tweet
+        /// </summary>
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The content of the tweet
+        /// </summary>
+        public string Content
+        {
+            get;
+            set;
+        }
+    }
+}

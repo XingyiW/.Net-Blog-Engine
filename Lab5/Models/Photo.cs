191a442f1703001282ad5243659aa78af08dfa14
XingyiW <46615477+XingyiW@users.noreply.github.com>
2019-05-05 21:33:41 -0400

    Add files via upload

---
+++ Lab5/Models/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.Models
+{
+    public class Photo
+    {
+        public int PhotoId
+        {
+            get;
+            set;
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public string Url
+        {
+            get;
+            set;
+        }
+
+    }
+}
